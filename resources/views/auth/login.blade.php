082c1b751239e6ae9b05564f4614e97ae4f54cb2
jordy-D-parra <parra26jordanys@gmail.com>
2026-03-20 10:41:10 -0400

    new file:   .editorconfig 	new file:   .env.example 	new file:   .gitattributes 	new file:   .gitignore 	new file:   README.md 	new file:   app/Http/Controllers/Controller.php 	new file:   app/Models/User.php 	new file:   app/Providers/AppServiceProvider.php 	new file:   artisan 	new file:   bootstrap/app.php 	new file:   bootstrap/cache/.gitignore 	new file:   bootstrap/providers.php 	new file:   composer.json 	new file:   composer.lock 	new file:   config/app.php 	new file:   config/auth.php 	new file:   config/cache.php 	new file:   config/database.php 	new file:   config/filesystems.php

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Básico</title>
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+</head>
+<body>
+    <H1>hola</H1>
+    <script src="{{ asset('js/login.js') }}"></script>
+</body>
+</html>
