b172d1f9bd1f49ed548de10a901facc15a0f9a2c
jordy-D-parra <parra26jordanys@gmail.com>
2026-04-10 17:23:45 -0400

    Actualizar migración de rol y limpiar archivo hot

--- database/migrations/2026_03_28_171946_01_create_rol_table.php
+++ database/migrations/2026_03_28_171946_01_create_rol_table.php
@@ -6,19 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::create('rol', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 50)->unique();
+            $table->text('descripcion')->nullable();
+            $table->integer('nivel')->default(1);
+            $table->boolean('es_activo')->default(true);
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('rol');
     }
 };
